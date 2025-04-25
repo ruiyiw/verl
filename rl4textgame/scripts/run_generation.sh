4f77670377a39552fc7f918d662d6df2f925044a
ruiyiw <ruiyiwang153@gmail.com>
2025-04-25 20:26:56 +0000

    Add CUDA flags

---
+++ rl4textgame/scripts/run_generation.sh
@@ -0,0 +1,22 @@
+set -x
+MODEL_PATH=Qwen/Qwen2.5-0.5B
+DATA_PATH=/workspace/local/full_traj_ppo_data
+export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7
+# export HYDRA_FULL_ERROR=1
+# Generation
+python3 -m verl.trainer.main_generation \
+    trainer.nnodes=1 \
+    trainer.n_gpus_per_node=7 \
+    data.path=$DATA_PATH/test.parquet \
+    data.prompt_key=prompt \
+    data.batch_size=8 \
+    data.n_samples=1 \
+    data.output_path=$DATA_PATH/test-output-8.parquet \
+    model.path=$MODEL_PATH \
+    rollout.temperature=0.6 \
+    rollout.top_p=0.95 \
+    rollout.prompt_length=2048 \
+    rollout.response_length=512 \
+    rollout.tensor_model_parallel_size=1 \
+    rollout.gpu_memory_utilization=0.7 \
+    rollout.max_num_batched_tokens=16384
