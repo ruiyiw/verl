994c2c92c4ff1e636fd928f3f84fa43618fcb547
ruiyiw <ruiyiwang153@gmail.com>
2025-04-26 00:17:07 +0000

    Add S3 data download to data processing

--- rl4textgame/data_preprocess.sh
+++ rl4textgame/data_preprocess.sh
@@ -1,26 +1,25 @@
 data_start=50001
 data_end=55000
 game_size=w2-o3-q4
-game_dir=/workspace/data/games/tiny_search
-local_dir=/workspace/local/
+s3_train_data_dir="s3://ruiyi-search-agents/ppo_data/full_traj_ppo_data/tiny_search/5000_data/gold-only_depth-0_max-full-traj-len-12/"
+s3_games_dir="s3://ruiyi-search-agents/ppo_data/game_data/tiny_search/"
+local_train_data_dir="../local/full_traj_ppo_data/"
+local_games_dir="../local/games/"
+local_parquet_dir="../local/full_traj_ppo_parquet/"
+
+# Download data from S3 bucket (should contain train.jsonl, validation.jsonl, and test.jsonl)
+mkdir -p ../local/
+aws s3 sync $s3_train_data_dir $local_train_data_dir
+# Download games from S3 bucket
+aws s3 sync $s3_games_dir $local_games_dir
 
 # Add training data and preprocess them into parquet
 python3 -m rl4textgame.data_process.data_preprocess \
     --task textworld \
     --dataset_id "${game_size}_${data_start}-${data_end}" \
-    --game_dir $game_dir \
-    --data_dir /workspace/data/full_traj_ppo_data/tiny_search/5000_data/gold-only_depth-0_max-full-traj-len-12 \
-    --local_dir $local_dir/full_traj_ppo_data
+    --game_dir $local_games_dir \
+    --data_dir $local_train_data_dir \
+    --local_dir $local_parquet_dir
 
-mkdir -p $local_dir/game_data/
-# Add train game data
-for i in $(seq $data_start $data_end); do
-    cp -n "${game_dir}/${game_size}_${i}.z8" $local_dir/game_data/
-done
-# Add validation and test game data
-for i in $(seq 1 50); do
-    cp -n "${game_dir}/${game_size}_$i.z8" $local_dir/game_data/
-done
-for i in $(seq 41001 41050); do
-    cp -n "${game_dir}/${game_size}_$i.z8" $local_dir/game_data/
-done
+# Remove .jsonl file
+rm -rf $local_train_data_dir
