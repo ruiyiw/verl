70fe92728e727de649bb70456ada720419fbb5aa
ruiyiw <ruiyiwang153@gmail.com>
2025-04-29 19:49:52 +0000

    Adding ppo trainer file

--- rl4textgame/scripts/run_generation.sh
+++ rl4textgame/scripts/run_generation.sh
@@ -1,12 +1,12 @@
 set -x
 MODEL_PATH=Qwen/Qwen2.5-0.5B
 DATA_PATH=/workspace/local/full_traj_ppo_data
-export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # export HYDRA_FULL_ERROR=1
 # Generation
 python3 -m verl.trainer.main_generation \
     trainer.nnodes=1 \
-    trainer.n_gpus_per_node=7 \
+    trainer.n_gpus_per_node=8 \
     data.path=$DATA_PATH/test.parquet \
     data.prompt_key=prompt \
     data.batch_size=8 \
@@ -18,5 +18,5 @@ python3 -m verl.trainer.main_generation \
     rollout.prompt_length=2048 \
     rollout.response_length=512 \
     rollout.tensor_model_parallel_size=1 \
-    rollout.gpu_memory_utilization=0.7 \
+    rollout.gpu_memory_utilization=0.8 \
     rollout.max_num_batched_tokens=16384
