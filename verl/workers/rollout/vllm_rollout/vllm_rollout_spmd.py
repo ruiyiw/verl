4a9a4a4092792668e55e235754caeba646deb264
ruiyiw <ruiyiwang153@gmail.com>
2025-05-30 22:21:57 +0000

    Fix bug of prompt/response_ids larger than max length

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -390,11 +390,11 @@ class vLLMRollout(BaseRollout):
             # Truncate input length if larger than max prompt length
             # Left padding
             batched_input_ids.append([self.pad_token_id] * (prompt_seq_len - new_seq_len) + next_input_ids_list[i]
-                                     if prompt_seq_len < new_seq_len else next_input_ids_list[:prompt_seq_len])
+                                     if prompt_seq_len > new_seq_len else next_input_ids_list[:prompt_seq_len])
             batched_attention_mask.append([0] * (prompt_seq_len - new_seq_len) + [1] * new_seq_len
-                                          if prompt_seq_len < new_seq_len else [1] * prompt_seq_len)
+                                          if prompt_seq_len > new_seq_len else [1] * prompt_seq_len)
             batched_position_ids.append([0] * (prompt_seq_len - new_seq_len) + list(range(new_seq_len))
-                                        if prompt_seq_len < new_seq_len else list(range(prompt_seq_len)))
+                                        if prompt_seq_len > new_seq_len else list(range(prompt_seq_len)))
         
         # Update multiturn_input
         multiturn_input = TensorDict(
